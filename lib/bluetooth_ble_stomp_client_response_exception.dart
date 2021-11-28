c3e314fac97462ab109b48d71d7bc47c0e620118
Elijah J. Passmore <elijahjpassmore@elijahjpassmore.com>
2021-11-28 14:06:22 +1300

    Add library line
    Signed-off-by: Elijah J. Passmore <elijahjpassmore@elijahjpassmore.com>

--- lib/bluetooth_ble_stomp_client_response_exception.dart
+++ lib/bluetooth_ble_stomp_client_response_exception.dart
@@ -1,3 +1,5 @@
+library bluetooth_ble_stomp_client;
+
 /// An exception raised after bad authentication responses.
 class BluetoothBleStompClientResponseException implements Exception {
   BluetoothBleStompClientResponseException({this.message});
