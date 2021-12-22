b6205724aa36d85ecdd1a44ea169396808a9f7e0
Elijah J. Passmore <elijahjpassmore@elijahjpassmore.com>
2021-12-23 08:11:52 +1300

    Add unknown STOMP status
    An unknown STOMP status may be useful is some scenarios.
    
    Signed-off-by: Elijah J. Passmore <elijahjpassmore@elijahjpassmore.com>

--- lib/bluetooth_ble_stomp_client_stomp_status.dart
+++ lib/bluetooth_ble_stomp_client_stomp_status.dart
@@ -2,6 +2,7 @@ library bluetooth_ble_stomp_client;
 
 /// The current status of the STOMP client.
 enum BluetoothBleStompClientStompStatus {
+  unknown,
   disconnected,
   unauthenticated,
   authenticating,
