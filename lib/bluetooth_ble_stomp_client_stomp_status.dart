7e2beee0dc45784d7609cc874c9f889f606a3b9f
Elijah J. Passmore <elijahjpassmore@elijahjpassmore.com>
2021-12-22 16:02:52 +1300

    Add unauthenticated to STOMP status enum
    Signed-off-by: Elijah J. Passmore <elijahjpassmore@elijahjpassmore.com>

--- lib/bluetooth_ble_stomp_client_stomp_status.dart
+++ lib/bluetooth_ble_stomp_client_stomp_status.dart
@@ -3,6 +3,7 @@ library bluetooth_ble_stomp_client;
 /// The current status of the STOMP client.
 enum BluetoothBleStompClientStompStatus {
   disconnected,
+  unauthenticated,
   authenticating,
   authenticated,
   connected,
